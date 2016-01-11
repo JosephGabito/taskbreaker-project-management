d861fea58eab68b36effcc9f9f963fbaf0e3113d
Joseph Gabito <codehaiku.io@gmail.com>
2016-01-11 16:17:24 +0800

    Changed thrive, thrive to task_breaker, task_breaker

--- templates/project-heading.php
+++ templates/project-heading.php
@@ -1,12 +1,12 @@
 <?php global $post; ?>
-<div id="thrive-single-project-group-details">
-	<div id="thrive-intranet-projects">
-		<ul id="thrive-projects-lists">
+<div id="task_breaker-single-project-group-details">
+	<div id="task_breaker-intranet-projects">
+		<ul id="task_breaker-projects-lists">
 			<li class="type-project">
-				<?php thrive_project_user( $post->post_author, $post->ID ); ?>
+				<?php task_breaker_project_user( $post->post_author, $post->ID ); ?>
 				
-				<div class="thrive-project-meta">
-					<?php thrive_project_meta( get_the_ID() ); ?>
+				<div class="task_breaker-project-meta">
+					<?php task_breaker_project_meta( get_the_ID() ); ?>
 				</div>
 			</li>
 		</ul>
