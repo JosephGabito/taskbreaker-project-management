9bddae33d234045f71886ae3eab59655a39a7a24
Joseph Gabito <codehaiku.io@gmail.com>
2016-01-11 16:16:01 +0800

    Changed Thrive to task_breaker

--- assets/js/dist/tasks/tasks-delete.js
+++ assets/js/dist/tasks/tasks-delete.js
@@ -1,5 +1,5 @@
  // Delete Task Single
- $('body').on('click', '#thrive-delete-btn', function() {
+ $('body').on('click', '#task_breaker-delete-btn', function() {
 
     var _delete_confirm = confirm("Are you sure you want to delete this task? This action is irreversible");
 
@@ -15,11 +15,11 @@
 
     var __http_params = {
 
-       action: 'thrive_transactions_request',
-       method: 'thrive_transaction_delete_ticket',
+       action: 'task_breaker_transactions_request',
+       method: 'task_breaker_transaction_delete_ticket',
        id: task_id,
        project_id: task_project_id,
-       nonce: thriveProjectSettings.nonce
+       nonce: task_breakerProjectSettings.nonce
 
    };
 
@@ -42,7 +42,7 @@
 
             location.href = "#tasks";
 
-            ThriveProjectView.switchView(null, '#thrive-project-tasks-context');
+            ThriveProjectView.switchView(null, '#task_breaker-project-tasks-context');
 
             $element.text('Delete');
 
@@ -51,7 +51,7 @@
        error: function() {
            ThriveProjectView.progress(false);
            location.href = "#tasks";
-           ThriveProjectView.switchView(null, '#thrive-project-tasks-context');
+           ThriveProjectView.switchView(null, '#task_breaker-project-tasks-context');
            $element.text('Delete');
 
        }
