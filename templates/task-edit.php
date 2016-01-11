78cb732a4b729dda4e4d16db8a5cc704607d8c4d
Joseph Gabito <codehaiku.io@gmail.com>
2016-01-11 16:17:50 +0800

    Changed thrive, thrive to task_breaker, task_breaker

--- templates/task-edit.php
+++ templates/task-edit.php
@@ -1,13 +1,13 @@
 <div class="form-wrap">
-	<div id="thrive-edit-task-message" class="thrive-notifier"></div>
+	<div id="task_breaker-edit-task-message" class="task_breaker-notifier"></div>
 
-	<input type="hidden" id="thriveTaskId" />
-	<div class="thrive-form-field">
-		<input placeholder="Task Title" type="text" id="thriveTaskEditTitle" maxlength="160" name="title" class="widefat"/>
-		<br><span class="description"><?php _e('Enter the title of this task. Max 160 characters', 'thrive'); ?></span>
+	<input type="hidden" id="task_breakerTaskId" />
+	<div class="task_breaker-form-field">
+		<input placeholder="Task Title" type="text" id="task_breakerTaskEditTitle" maxlength="160" name="title" class="widefat"/>
+		<br><span class="description"><?php _e('Enter the title of this task. Max 160 characters', 'task_breaker'); ?></span>
 	</div><br/>
 	
-	<div class="thrive-form-field">
+	<div class="task_breaker-form-field">
 		<?php 
 			$args = array(
 				'teeny' => true,
@@ -15,23 +15,23 @@
 				'media_buttons' => false
 			); 
 		?>
-		<?php echo wp_editor($content = null, $editor_id = "thriveTaskEditDescription", $args); ?>
-		<br><span class="description"><?php _e('In few words, explain what this task is all about', 'thrive'); ?></span>
+		<?php echo wp_editor($content = null, $editor_id = "task_breakerTaskEditDescription", $args); ?>
+		<br><span class="description"><?php _e('In few words, explain what this task is all about', 'task_breaker'); ?></span>
 	</div><br/>
 
-	<div class="thrive-form-field">
-		<label for="thrive-task-priority-select">
-			<strong><?php _e('Priority:', 'thrive'); ?> </strong>
-			<?php echo thrive_task_priority_select($default = 1, $name = 'thrive-task-edit-priority', $id='thrive-task-edit-select-id'); ?>
+	<div class="task_breaker-form-field">
+		<label for="task_breaker-task-priority-select">
+			<strong><?php _e('Priority:', 'task_breaker'); ?> </strong>
+			<?php echo task_breaker_task_priority_select($default = 1, $name = 'task_breaker-task-edit-priority', $id='task_breaker-task-edit-select-id'); ?>
 		</label>
 	</div>
 
-	<div class="thrive-form-field">
-		<button id="thrive-delete-btn" class="button button-primary button-large" style="float:right; margin-left: 10px;">
+	<div class="task_breaker-form-field">
+		<button id="task_breaker-delete-btn" class="button button-primary button-large" style="float:right; margin-left: 10px;">
 			<?php _e('Delete', 'dunhakdis'); ?>
 		</button>
 
-		<button id="thrive-edit-btn" class="button button-primary button-large" style="float:right">
+		<button id="task_breaker-edit-btn" class="button button-primary button-large" style="float:right">
 			<?php _e('Update Task', 'dunhakdis'); ?>
 		</button>
 		
