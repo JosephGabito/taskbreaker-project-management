8e13601a02044f0cc6669a70f5b5935820ffdf96
Joseph Gabito <codehaiku.io@gmail.com>
2016-01-11 16:17:34 +0800

    Changed thrive, thrive to task_breaker, task_breaker

--- templates/task-add.php
+++ templates/task-add.php
@@ -7,34 +7,34 @@
 ?>
 <div class="form-wrap">
 	
-	<div id="thrive-add-task-message" class="thrive-notifier"></div>
+	<div id="task_breaker-add-task-message" class="task_breaker-notifier"></div>
 
-	<div class="thrive-form-field">
-		<input placeholder="Task Title" type="text" id="thriveTaskTitle" maxlength="160" name="title" class="widefat"/>
-		<br><span class="description"><?php _e('Enter the title of this task. Max 160 characters', 'thrive'); ?></span>
+	<div class="task_breaker-form-field">
+		<input placeholder="Task Title" type="text" id="task_breakerTaskTitle" maxlength="160" name="title" class="widefat"/>
+		<br><span class="description"><?php _e('Enter the title of this task. Max 160 characters', 'task_breaker'); ?></span>
 	</div><br/>
 	
-	<div class="thrive-form-field">
+	<div class="task_breaker-form-field">
 		<?php $args = array(
 			'teeny' => true,
 			'editor_height' => 100,
 			'media_buttons' => false,
 		); ?>
 		
-		<?php echo wp_editor($content = null, $editor_id = "thriveTaskDescription", $args); ?>
+		<?php echo wp_editor($content = null, $editor_id = "task_breakerTaskDescription", $args); ?>
 
-		<br><span class="description"><?php _e('In few words, explain what this task is all about', 'thrive'); ?></span>
+		<br><span class="description"><?php _e('In few words, explain what this task is all about', 'task_breaker'); ?></span>
 	</div><br />
 
-	<div class="thrive-form-field">
-		<label for="thrive-task-priority-select">
-			<strong><?php _e('Priority:', 'thrive'); ?> </strong>
-			<?php echo thrive_task_priority_select(); ?>
+	<div class="task_breaker-form-field">
+		<label for="task_breaker-task-priority-select">
+			<strong><?php _e('Priority:', 'task_breaker'); ?> </strong>
+			<?php echo task_breaker_task_priority_select(); ?>
 		</label>
 	</div>
 
-	<div class="thrive-form-field">
-		<button id="thrive-submit-btn" class="button button-primary button-large" style="float:right">
+	<div class="task_breaker-form-field">
+		<button id="task_breaker-submit-btn" class="button button-primary button-large" style="float:right">
 			<?php _e('Save Task', 'dunhakdis'); ?>
 		</button>
 		<div style="clear:both"></div>
