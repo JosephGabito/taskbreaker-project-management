099680ea972335d44ff70b4b2e518c55da2e929a
Joseph Gabito <codehaiku.io@gmail.com>
2016-01-11 16:16:52 +0800

    Changed thrive, thrive to task_breaker, task_breaker

--- core/enqueue.php
+++ core/enqueue.php
@@ -1,13 +1,13 @@
 <?php
 
-add_action( 'admin_head',      		 'thrive_admin_stylesheet' );
-add_action( 'admin_print_scripts',   'thrive_admin_scripts' );
-add_action( 'wp_enqueue_scripts',    'thrive_register_scripts' );
-add_action( 'wp_footer', 			 'thrive_register_config' );
+add_action( 'admin_head',      		 'task_breaker_admin_stylesheet' );
+add_action( 'admin_print_scripts',   'task_breaker_admin_scripts' );
+add_action( 'wp_enqueue_scripts',    'task_breaker_register_scripts' );
+add_action( 'wp_footer', 			 'task_breaker_register_config' );
 // Disable login modals introduced in WordPress 3.6
 remove_action( 'admin_enqueue_scripts', 'wp_auth_check_load' );
 
-function thrive_admin_stylesheet() {
+function task_breaker_admin_stylesheet() {
 
 	global $post;
 
@@ -19,12 +19,12 @@ function thrive_admin_stylesheet() {
 		return;
 	}
 
-	wp_enqueue_style( 'thrive-admin-style', plugin_dir_url( __FILE__ ) . '../assets/css/admin.css' );
+	wp_enqueue_style( 'task-breaker-admin-style', TASK_BREAKER_ASSET_URL . '/assets/css/admin.css' );
 
 	return;
 }
 
-function thrive_admin_scripts() {
+function task_breaker_admin_scripts() {
 
 	global $post;
 
@@ -35,7 +35,7 @@ function thrive_admin_scripts() {
 	if ( "project" === $post->post_type ) {
 
 		wp_enqueue_script( 'backbone' );
-		wp_enqueue_script( 'thrive-admin', plugin_dir_url( __FILE__ ) . '../assets/js/admin.js', array( 'jquery', 'backbone' ), $ver = 1.0, $in_footer = true );
+		wp_enqueue_script( 'task_breaker-admin', TASK_BREAKER_ASSET_URL . '/assets/js/admin.js', array( 'jquery', 'backbone' ), $ver = 1.0, $in_footer = true );
 		// Deregister the culprit.
 		wp_deregister_script( 'vc_accordion_script' );
 		
@@ -45,43 +45,42 @@ function thrive_admin_scripts() {
 
 }
 
-function thrive_register_scripts() {
+function task_breaker_register_scripts() {
 
 	// Front-end stylesheet.
-	wp_enqueue_style( 'thrive-stylesheet', plugin_dir_url( __FILE__ ) . '../assets/css/style.css', array(), 1.0 );
+	wp_enqueue_style( 'task_breaker-stylesheet', TASK_BREAKER_ASSET_URL . '/css/style.css', array(), 1.0 );
 
 	// Administrator JS.
 	if ( is_admin() ) {
-		wp_enqueue_script(
-			'thrive-admin',  plugin_dir_url( __FILE__ ) . '../assets/js/admin.js', array( 'jquery', 'backbone' ),  // Dependencies.
+		wp_enqueue_script( 'task_breaker-admin',  TASK_BREAKER_ASSET_URL . '/js/admin.js', array( 'jquery', 'backbone' ),  // Dependencies.
 			1.0, true
 		);
 	}
 
 	// Front-end JS.
-	if ( is_singular( THRIVE_PROJECT_SLUG ) ) {
-		wp_enqueue_script(
-			'thrive-js', plugin_dir_url( __FILE__ ) . '../assets/js/thrive.min.js', array( 'jquery', 'backbone' ),
+	if ( is_singular( TASK_BREAKER_PROJECT_SLUG ) ) {
+		wp_enqueue_script('task_breaker-js', 
+			TASK_BREAKER_ASSET_URL . 'js/task-breaker.min.js', array( 'jquery', 'backbone' ),
 			1.0, true
 		);
 	}
 
-	// Project Archive JS.
-	wp_enqueue_script(
-		'thrive-archive-js', plugin_dir_url( __FILE__ ) . '../assets/js/archive.js', array( 'jquery', 'backbone' ),
-		1.0, true
-	);
+		// Project Archive JS.
+		wp_enqueue_script( 'task_breaker-archive-js', 
+			TASK_BREAKER_ASSET_URL . 'js/archive.js', array( 'jquery', 'backbone' ),
+			1.0, true
+		);
 
 	return;
 }
 
-function thrive_register_config() {
+function task_breaker_register_config() {
 
-	if ( is_singular( THRIVE_PROJECT_SLUG ) ) { ?>
+	if ( is_singular( TASK_BREAKER_PROJECT_SLUG ) ) { ?>
 		<script>
 			<?php global $post; ?>
-			var thriveAjaxUrl = '<?php echo admin_url( 'admin-ajax.php' ); ?>';
-			var thriveTaskConfig = {
+			var task_breakerAjaxUrl = '<?php echo admin_url( 'admin-ajax.php' ); ?>';
+			var task_breakerTaskConfig = {
 				currentProjectId: '<?php echo $post->ID; ?>',
 				currentUserId: '<?php echo get_current_user_id(); ?>',
 			}
