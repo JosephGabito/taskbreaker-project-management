22bbc854d4e45857b5b7acc8797bb6ed607d32ab
Joseph Gabito <codehaiku.io@gmail.com>
2016-01-11 16:15:32 +0800

    Changed Thrive to task_breaker

--- assets/js/archive.js
+++ assets/js/archive.js
@@ -1,24 +1,24 @@
 jQuery(document).ready(function($){
 	// Modal
-	$('#thrive-new-project-btn').on( 'click', function( e ) {
+	$('#task_breaker-new-project-btn').on( 'click', function( e ) {
 		e.preventDefault();
-		$('#thrive-new-project-modal').show();
-		$('#thrive-modal-content').toggleClass('active');
+		$('#task_breaker-new-project-modal').show();
+		$('#task_breaker-modal-content').toggleClass('active');
 
 	});
 	
-	$('#thrive-new-project-modal').on( 'click', function( e ) {
+	$('#task_breaker-new-project-modal').on( 'click', function( e ) {
 		if ( e.target !== this ) {
 			return;
 		}
 		$(this).hide();
-		$('#thrive-modal-content').toggleClass('active');
+		$('#task_breaker-modal-content').toggleClass('active');
 	});
 
 	$(document).keyup(function(e) {
 	  	if ( e.keyCode == 27 ) {
-			$('#thrive-new-project-modal').hide();
-			$('#thrive-modal-content').toggleClass('active');
+			$('#task_breaker-new-project-modal').hide();
+			$('#task_breaker-modal-content').toggleClass('active');
 	  	}
 	});
 
