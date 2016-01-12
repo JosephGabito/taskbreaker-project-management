dfb21b70dc026d2c9f48209393b8a9c37c8048d6
Joseph Gabito <codehaiku.io@gmail.com>
2016-01-12 16:13:01 +0800

    changed comment_ticket to comment_ticket_id

--- assets/js/dist/comments/comment-add.js
+++ assets/js/dist/comments/comment-add.js
@@ -1,12 +1,12 @@
   $('body').on('click', '#updateTaskBtn', function() {
 
-      var comment_ticket = ThriveProjectModel.id,
+      var comment_ticket_id = ThriveProjectModel.id,
           comment_details = $('#task-comment-content').val(),
           task_priority = $('#task_breaker-task-priority-update-select').val(),
           comment_completed = $('input[name=task_commment_completed]:checked').val(),
           task_project_id = parseInt( ThriveProjectModel.project_id );
 
-      if (0 === comment_ticket) {
+      if (0 === comment_ticket_id) {
           return;
       }
 
@@ -20,7 +20,7 @@
       var __http_params = {
           action: 'task_breaker_transactions_request',
           method: 'task_breaker_transaction_add_comment_to_ticket',
-          ticket_id: comment_ticket,
+          ticket_id: comment_ticket_id,
           priority: task_priority,
           details: comment_details,
           completed: comment_completed,
@@ -67,11 +67,8 @@
                   $('#task_breaker-comment-completed-radio').addClass('hide');
 
               }
-
-             // console.log(response.stats);
-
+              // console.log(response.stats);
               ThriveProjectView.updateStats( response.stats );
-              
           },
           error: function() {
 
