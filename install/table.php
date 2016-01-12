0782e80e4ba19c3929ed6ff5d852d2e9213942fa
Joseph Gabito <codehaiku.io@gmail.com>
2016-01-12 20:08:19 +0800

    Added Migration Script.

--- install/table.php
+++ install/table.php
@@ -4,7 +4,7 @@
  * This file implements our install script for projects
  * and task table. Runs on plugin activation.
  * 
- * @package ThriveIntranet
+ * @package TaskBreaker
  * @since   1.0
  */
 
@@ -14,8 +14,8 @@
 define( 'TASK_BREAKER_TASKS_TABLE', 'task_breaker_tasks' );
 define( 'TASK_BREAKER_COMMENTS_TABLE', 'task_breaker_comments' );
 
-define( 'TASK_BREAKER_TASKS_TABLE_VERSION', '1.0' );
-define( 'TASK_BREAKER_COMMENTS_TABLE_VERSION', '1.0' );
+define( 'TASK_BREAKER_TASKS_TABLE_VERSION', '1.2.1122016' );
+define( 'TASK_BREAKER_COMMENTS_TABLE_VERSION', '1.2.1122016' );
 
 function task_breaker_install() 
 {
@@ -25,6 +25,17 @@ function task_breaker_install()
 
 	// Setup our tasks comments/discussion table.
 	task_breaker_comments_setup_table();
+
+	return;
+}
+
+function task_breaker_import_thrive_intranet_data() {
+
+	// Import existing tasks.
+	task_breaker_import_thrive_tasks();
+
+	// Import existing comments.
+	task_breaker_import_thrive_comments();
 
 	return;
 }
@@ -52,7 +63,7 @@ function task_breaker_tasks_setup_table()
   		priority int(2) NOT NULL,
   		completed_by int(10) NOT NULL,
   		date_created timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  		PRIMARY KEY (id)
+  		UNIQUE KEY (id)
 	) $charset_collate ;";
 
 	// Include the 'upgrade' script inside WP include dir.
@@ -86,9 +97,9 @@ function task_breaker_comments_setup_table()
 		  	ticket_id int(10) NOT NULL,
 		  	status int(11) NOT NULL DEFAULT '0',
 		  	date_added timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-		  	PRIMARY KEY (id)
+		  	UNIQUE KEY (id)
 		) $charset_collate";
-
+	
 	// Include the 'upgrade' script inside WP include dir.
 	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
 
@@ -96,6 +107,66 @@ function task_breaker_comments_setup_table()
 	dbDelta( $comments_table_structure );
 
 	add_option( 'task_breaker_comments_table_version', TASK_BREAKER_COMMENTS_TABLE_VERSION );
+
+	return;
+}
+
+function task_breaker_import_thrive_tasks() {
+
+	global $wpdb;
+
+	$tasks_table_name = $wpdb->prefix . TASK_BREAKER_TASKS_TABLE;
+
+	// Check if it has data in wp_thrive_tasks.
+	// Copy the data if it has.
+	$old_table = $wpdb->prefix . 'thrive_tasks';
+	$has_table = $wpdb->get_results("SHOW TABLES LIKE '".$old_table."'");
+
+	// Only run once
+	if ( ! get_option( "task_breaker_tasks_imported" ) ) {
+
+		if ( $has_table ) {
+			
+			$wpdb->query( "INSERT {$tasks_table_name} SELECT * FROM {$old_table};" );
+			
+			update_option( "task_breaker_tasks_imported", "yes" );
+		
+			// Update the meta
+			$wpdb->update( 
+				$wpdb->postmeta, 
+				array( 
+					'meta_key' => 'task_breaker_project_group_id',	// string
+				), 
+				array( 'meta_key' => 'thrive_project_group_id' ), 
+				array( 
+					'%s',	// value1
+				), 
+				array( '%s' ) 	
+			);
+		}
+		
+	}
+
+	return;
+}
+
+function task_breaker_import_thrive_comments() {
+
+	global $wpdb;
+
+	$comments_table_name = $wpdb->prefix . TASK_BREAKER_COMMENTS_TABLE;
+
+	// Check if it has data in wp_thrive_comments.
+	// Copy the data if it has.
+	$old_table = $wpdb->prefix . 'thrive_comments';
+	$has_table = $wpdb->get_results("SHOW TABLES LIKE '".$old_table."'");
+	
+	if ( ! get_option( "task_breaker_comments_imported" ) ) {
+		if ( $has_table ) {
+			$wpdb->query( "INSERT {$comments_table_name} SELECT * FROM {$old_table};" );
+			update_option( "task_breaker_comments_imported", "yes" );
+		}
+	}
 
 	return;
 }
