38cf166236f5195687f473d826160c7e607fa002
Joseph Gabito <codehaiku.io@gmail.com>
2016-02-04 19:04:04 +0800

    Move project modal

---
+++ templates/project-add-modal.php
@@ -0,0 +1,48 @@
+<a id="task_breaker-new-project-btn" class="button" href="#">
+
+    <?php _e( 'New Project', 'task_breaker' ); ?>
+
+</a>
+
+<div class="clearfix"></div>
+
+
+<div id="task_breaker-new-project-modal">
+
+    <div id="task_breaker-modal-content">
+
+        <div id="task_breaker-modal-heading">
+
+            <h5 class="alignleft">
+
+                <?php _e( 'Add New Project', 'task_breaker' ); ?>
+
+            </h5>
+
+            <span id="task_breaker-modal-close" class="alignright">
+                &times;
+            </span>
+
+            <div class="clearfix"></div>
+
+        </div>
+
+        <div id="task_breaker-modal-body">
+
+            <?php task_breaker_new_project_form( $group_id ); ?>
+
+        </div>
+
+        <div id="task_breaker-modal-footer">
+
+            <small>
+
+                <?php _e( "Tip: Press the <em>'escape'</em> key in your keyboard to hide this form", 'task_breaker' ); ?>
+
+            </small>
+
+        </div>
+
+    </div>
+
+</div>
