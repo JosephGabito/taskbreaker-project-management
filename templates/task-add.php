ac6e1ba821ae5a06c78188a028925e76643c6e5c
Joseph Gabito <codehaiku.io@gmail.com>
2016-02-04 19:04:22 +0800

    Moved Task Add Template

--- templates/task-add.php
+++ templates/task-add.php
@@ -6,21 +6,21 @@
  */
 ?>
 <div class="form-wrap">
-	
+
 	<div id="task_breaker-add-task-message" class="task_breaker-notifier"></div>
 
 	<div class="task_breaker-form-field">
 		<input placeholder="Task Title" type="text" id="task_breakerTaskTitle" maxlength="160" name="title" class="widefat"/>
 		<span class="description"><?php _e('Enter the title of this task. Max 160 characters', 'task_breaker'); ?></span>
 	</div>
-	
+
 	<div class="task_breaker-form-field">
 		<?php $args = array(
 			'teeny' => true,
 			'editor_height' => 100,
 			'media_buttons' => false,
 		); ?>
-		
+
 		<?php echo wp_editor($content = null, $editor_id = "task_breakerTaskDescription", $args); ?>
 		<span class="description">
 			<br>
