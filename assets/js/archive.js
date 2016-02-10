99e289744398def7e1f06c5f618171b2ee87a6dc
Joseph Gabito <codehaiku.io@gmail.com>
2016-02-10 15:57:16 +0800

    Added initial commit to conditional statements

--- assets/js/archive.js
+++ assets/js/archive.js
@@ -6,7 +6,7 @@ jQuery(document).ready(function($){
 		$('#task_breaker-modal-content').toggleClass('active');
 
 	});
-	
+
 	$('#task_breaker-new-project-modal').on( 'click', function( e ) {
 		if ( e.target !== this ) {
 			return;
@@ -22,4 +22,65 @@ jQuery(document).ready(function($){
 	  	}
 	});
 
+
+	// Add project
+	$('#task_breakerSaveProjectBtn').on( 'click', function(e){
+
+		e.preventDefault();
+
+		$('#project-add-modal-js-message').html("");
+
+		var form_error_count = 0;
+
+		var project_details = {
+			title: $('#task_breaker-project-name').val(),
+			content: $('#task_breaker-project-content').val(),
+			group_id: $('#task_breaker-project-assigned-group').val(),
+
+			method: $('#task_breaker-project-add-new-form input[name=method]').val(),
+			action: $('#task_breaker-project-add-new-form input[name=action]').val(),
+			_wp_http_referer: $('#task_breaker-project-add-new-form input[name=_wp_http_referer]').val(),
+			nonce: $('#nonce').val()
+		};
+
+		console.log(project_details);
+
+		for ( key in project_details ) {
+
+			var field_value = project_details[key].trim();
+			var field_length = field_value.length;
+
+			if ( 0 === field_length ) {
+				form_error_count++;
+			}
+		}
+
+		if ( form_error_count >= 1 ) {
+			$('#project-add-modal-js-message').html(
+				"<p id='message' class='error'>All fields are required</p>"
+			).removeClass('hide');
+		} else {
+
+			console.log('Requesting Project...');
+
+			$.ajax({
+			  type: "POST",
+			  dataType: 'json',
+			  url: $('#task_breaker-project-add-new-form-form').attr('action'),
+			  data: project_details,
+			  success: function( response ) {
+				  $('#project-add-modal-js-message').html(
+					  "<p id='message' class='success'>Project successfully added. Redirecting you in few seconds...</p>"
+				  ).removeClass('hide');
+				  location.href = response.project_permalink;
+			  },
+			  error: function(err, message) {
+				  console.log(err);
+				  console.log(message);
+			  }
+			});
+
+		}
+
+	});
 });
