cb2df6d73e66308d9407f5d0abb435eb86445cd9
Joseph Gabito <codehaiku.io@gmail.com>
2016-02-10 19:26:42 +0800

    Project Loop

--- templates/project-loop.php
+++ templates/project-loop.php
@@ -2,18 +2,18 @@
 /**
  * Fires at the top of the members directory template file.
  *
- * @since ThriveIntranet 1.0
+ * @since Task Breaker 1.0
  */
 do_action( 'task_breaker_before_projects_directory' ); ?>
 
 <div id="buddypress">
-	
+
 	<div id="task_breaker-intranet-projects">
-		
+
 		<?php task_breaker_new_project_modal(); ?>
 
 		<?php task_breaker_project_loop( array() ); ?>
-		
+
 	</div><!--#task_breaker-intranet-projects-->
 
 </div><!-- #buddypress -->
