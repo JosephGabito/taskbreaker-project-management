600698699fbde65ddd0f3f6543b45601583f24f8
Joseph Gabito <codehaiku.io@gmail.com>
2016-02-22 20:22:39 +0800

    Updated Version

--- task-breaker.php
+++ task-breaker.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Task Breaker
  * Description: A WordPress plug-in that will help you break some task!
- * Version: 1.0.3
+ * Version: 1.0.4
  * Author: Dunhakdis
  * Author URI: http://dunhakdis.me
  * Text Domain: task_breaker
