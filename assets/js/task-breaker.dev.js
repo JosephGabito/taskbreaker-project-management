e6cf8f85232255600b366a12815fa3e7b66455b7
Joseph Gabito <codehaiku.io@gmail.com>
2016-07-26 22:12:56 +0800

    ...

--- assets/js/task-breaker.dev.js
+++ assets/js/task-breaker.dev.js
@@ -167,6 +167,7 @@ var __ThriveProjectView = Backbone.View.extend({
                 var taskEditor = tinymce.get('task_breakerTaskEditDescription');
 
                 $('#task_breakerTaskId').val(task.id).removeAttr("disabled");
+
                 $('#task_breakerTaskEditTitle').val(task.title).removeAttr("disabled");
 
                 if ( taskEditor ) {
@@ -174,6 +175,18 @@ var __ThriveProjectView = Backbone.View.extend({
                 } else {
                     $( '#task_breakerTaskEditDescription' ).val( task.description );
                 }
+
+                $("#task-user-assigned-edit").val('');
+
+                $.each( task.assign_users_meta.members_stack, function( key, val ) {
+                    var option = document.createElement("option");
+                        option.value = val.ID;
+                        option.text  = val.display_name;
+                        option.selected  = "selected";
+                        document.getElementById("task-user-assigned-edit").appendChild( option );
+                });
+
+                __this.autoSuggestMembers( $("#task-user-assigned-edit"), true, task );
 
                 $( "#task_breaker-task-edit-select-id" ).val( task.priority ).change().removeAttr("disabled");
 
@@ -303,6 +316,57 @@ var __ThriveProjectView = Backbone.View.extend({
             width: Math.ceil( ( ( stats.completed / stats.total ) * 100 ) ) + '%'
         });
 
+    },
+
+    autoSuggestMembers: function( selectElement, clearSelect, task ) {
+
+        if ( ! selectElement ) {
+            return;
+        }
+
+        var $resultTemplate = function( result ) {
+
+			if ( result.avatar ) {
+
+			    var $state = $('<span><img class="result-template-avatar" src="'+result.avatar+'" alt="s" />'+result.text+'</span>');
+			}
+
+			return $state;
+		}
+
+
+		selectElement.select2({
+			maximumInputLength: 20,
+			placeholder: "Type member\'s name...",
+			allowClear: true,
+			minimumResultsForSearch: Infinity,
+			minimumInputLength: 2,
+			tag: true,
+			ajax: {
+
+				data: function ( params ) {
+
+					var query = {
+						action: 'task_breaker_transactions_request',
+						method: 'task_breaker_transactions_user_suggest',
+						nonce: task_breakerProjectSettings.nonce,
+						group_id: task_breakerProjectSettings.current_group_id,
+						term: params.term,
+						user_id_collection: 0
+					}
+
+					if ( selectElement.val() ) {
+						query.user_id_collection = selectElement.val();
+					}
+
+					return query;
+				},
+				url: task_breakerAjaxUrl,
+				delay: 250,
+				cache: true
+			},
+			templateResult: $resultTemplate
+		});
     }
 });
 
@@ -342,7 +406,10 @@ var __ThriveProjectRoute = Backbone.Router.extend({
     },
     add: function() {
         this.view.switchView(null, '#task_breaker-project-add-new-context');
+
         $('#task_breaker-project-add-new').css('display', 'block');
+        $('#task-user-assigned').val("");
+        this.view.autoSuggestMembers( $("#task-user-assigned"), true, null );
 
         if ( tinymce.editors.task_breakerTaskDescription ) {
             tinymce.editors.task_breakerTaskDescription.setContent('');
@@ -885,4 +952,4 @@ $('body').on('click', '#task_breakerUpdateProjectBtn', function() {
  });
 
 }); // end $(window).load();
-}); // end jQuery(document).ready(); 
+}); // end jQuery(document).ready();
