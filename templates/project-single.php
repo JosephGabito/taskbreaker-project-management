e8d49b5eea2472b4b7c72640f3a3ef7ff75c5900
Joseph Gabito <codehaiku.io@gmail.com>
2017-10-03 13:28:01 +0800

    fixe language

--- templates/project-single.php
+++ templates/project-single.php
@@ -13,6 +13,13 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	return;
 }
+
+if ( ! function_exists('bp_is_active') ) {
+	echo '<div id="message" class="info">';
+		esc_html_e( 'Please install and activate BuddyPress to use this feature.', 'task_breaker' );
+	echo '</div>';
+	return;
+}
 ?>
 
 <?php if ( bp_is_active( 'groups' ) ) { ?>
@@ -28,34 +35,34 @@ if ( ! defined( 'ABSPATH' ) ) {
 		<?php if ( $user_access->can_view_project( $args->ID ) ) { ?>
 
 			<?php include $core->get_template_directory() . '/project-heading.php'; ?>
-
+			
 			<div class="task_breaker-project-tabs">
 
 				<ul id="task_breaker-project-tab-li">
 					<li class="task_breaker-project-tab-li-item active">
 						<a data-content="task_breaker-project-dashboard" class="task_breaker-project-tab-li-item-a" href="#tasks/dashboard">
-							<?php esc_html_e( 'Dashboard', 'task_breaker' ); ?>
+							<?php esc_html_e( 'Dashboard', 'task-breaker' ); ?>
 						</a>
 					</li>
 					<li class="task_breaker-project-tab-li-item">
 						<a data-content="task_breaker-project-tasks" class="task_breaker-project-tab-li-item-a" href="#tasks">
-							<?php esc_html_e( 'Tasks', 'task_breaker' ); ?>
+							<?php esc_html_e( 'Tasks', 'task-breaker' ); ?>
 						</a>
 					</li>
 					<li class="task_breaker-project-tab-li-item">
 						<a data-content="task_breaker-project-add-new" id="task_breaker-project-add-new" class="task_breaker-project-tab-li-item-a" href="#tasks/add">
-							<?php esc_html_e( 'Add New', 'task_breaker' ); ?>
+							<?php esc_html_e( 'Add New', 'task-breaker' ); ?>
 						</a>
 					</li>
 					<li class="task_breaker-project-tab-li-item">
 						<a data-content="task_breaker-project-edit" id="task_breaker-project-edit-tab" class="task_breaker-project-tab-li-item-a" href="#">
-							<?php esc_html_e( 'Edit', 'task_breaker' ); ?>
+							<?php esc_html_e( 'Edit', 'task-breaker' ); ?>
 						</a>
 					</li>
 					<?php if ( $user_access->can_edit_project( $__post->ID ) ) { ?>
 						<li class="task_breaker-project-tab-li-item">
 							<a data-content="task_breaker-project-settings" class="task_breaker-project-tab-li-item-a" href="#tasks/settings">
-								<?php esc_html_e( 'Settings', 'task_breaker' ); ?>
+								<?php esc_html_e( 'Settings', 'task-breaker' ); ?>
 							</a>
 						</li>
 					<?php } ?>
@@ -75,7 +82,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 			<div id="task-breaker-access-project-not-allowed" class="row">
 				<div class="col-xs-12">
 					<div class="task-breaker-message info">
-						<?php esc_attr_e( 'This project can only be accessed by group members. Use the button below join the group and receive an access to this project.', 'task_breaker' ); ?>
+						<?php esc_attr_e( 'This project can only be accessed by group members. Use the button below join the group and receive an access to this project.', 'task-breaker' ); ?>
 					</div>
 				</div>
 			</div>
@@ -86,14 +93,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 			<?php $join_link = wp_nonce_url( bp_get_group_permalink( $group ) . 'join', 'groups_join_group' ); ?>
 
-			<a class="button" href="<?php echo esc_url( $join_link ); ?>" title="<?php esc_attr_e( 'Join Group', 'task_breaker' ); ?>">
-				<?php esc_attr_e( 'Join Group', 'task_breaker' ); ?>
+			<a class="button" href="<?php echo esc_url( $join_link ); ?>" title="<?php esc_attr_e( 'Join Group', 'task-breaker' ); ?>">
+				<?php esc_attr_e( 'Join Group', 'task-breaker' ); ?>
 			</a>
 
 		<?php } ?>
 	</div><!--#task_breaker-project-->
 <?php } else { ?>
 	<p id="message" class="info">
-		<?php _e( 'Please enable BuddyPress Groups Components.', 'task_breaker' ); ?>
+		<?php _e( 'Please enable BuddyPress Groups Components.', 'task-breaker' ); ?>
 	</p>
 <?php } ?>
