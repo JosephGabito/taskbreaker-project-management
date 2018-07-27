14a80669e64bd3af72262b079fbbe7fac640d7ce
Joseph Gabito <codehaiku.io@gmail.com>
2018-07-27 11:57:30 +0800

    Task Deadline Started..

--- assets/js/dist/tasks/tasks-add.js
+++ assets/js/dist/tasks/tasks-add.js
@@ -28,6 +28,7 @@ $('#task_breaker-submit-btn').click(function(e) {
             method: 'task_breaker_transaction_add_ticket',
 
             description: taskDescription,
+            deadline: $('#js-taskbreaker-task-deadline').val(),
 
             title: $('#task_breakerTaskTitle').val(),
             milestone_id: $('#task_breakerTaskMilestone').val(),
