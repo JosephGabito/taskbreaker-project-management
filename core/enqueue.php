4c93e61d7c07a1554dbbce4f37023e1385f9cacf
Joseph Gabito <codehaiku.io@gmail.com>
2018-08-10 16:19:39 +0800

    localized script

--- core/enqueue.php
+++ core/enqueue.php
@@ -61,12 +61,37 @@ final class TaskBreakerEnqueue {
 
 		// Front-end JS.
 		if ( is_singular( TASK_BREAKER_PROJECT_SLUG ) ) {
+
 			wp_enqueue_script(
 				'task_breaker-js',
 				TASK_BREAKER_ASSET_URL . 'js/task-breaker.min.js',
 				array( 'jquery', 'backbone' ),
 				$this->version, true
 			);
+
+			// Localize TB js strings.
+			$translation_array = array(
+				'file_error' => __( 'There was an error uploading your file. File size exceeded the allowed number of bytes per request', 'taskbreaker-project-management' ),
+				'file_attachment_error' => __('The application did not received any response from the server. Try uploading smaller files.', 'taskbreaker-project-management'),
+				'tasks_not_found' => __('No tasks found. Try different keywords and filters.','taskbreaker-project-management'),
+				'comment_confirm_delete' => __('Are you sure you want to delete this comment? This action is irreversible.', 'taskbreaker-project-management'),
+				'comment_error' => __('Transaction Error: There was an error trying to delete this comment.', 'taskbreaker-project-management'),
+				'project_confirm_delete' => __('Are you sure you want to delete this project? All the tickets under this project will be deleted as well. This action cannot be undone.', 'taskbreaker-project-management'),
+				'project_error' => __('There was an error trying to delete this post. Try again later.', 'taskbreaker-project-management'),
+				'project_label_btn_update' => __('Update Project', 'taskbreaker-project-management'),
+				'project_authentication_error' => __('Only group administrators and moderators can update the project settings.', 'taskbreaker-project-management'),
+				'project_all_fields_required' => __('There was an error saving the project. All fields are required.', 'taskbreaker-project-management'),
+				'task_error_500' => __('Unexpected Error (500)', 'taskbreaker-project-management'),
+				'task_unexpected_error' => __('Unexpected Error Encountered During Request', 'taskbreaker-project-management'),
+				'task_confirm_delete' => __('Are you sure you want to delete this task? This action is irreversible', 'taskbreaker-project-management'),
+				'task_updated' => __('Task successfully updated', 'taskbreaker-project-management'),
+				'task_view' => __('View', 'taskbreaker-project-management'),
+				'task_update_error' => __('There was an error updating the task. All fields are required.', 'taskbreaker-project-management'),
+				'task_unauthorized_error' => __('You are not allowed to modify this task. Only group project administrators and group projects moderators are allowed.', 'taskbreaker-project-management'),
+				'file_attachment_delete' => __('Are you sure you want to delete this file attachment? This process is not reversible.', 'taskbreaker-project-management')
+			);
+
+			wp_localize_script( 'task_breaker-js', 'taskbreaker_strings', $translation_array );
 
 			wp_enqueue_script(
 				'task_breaker-select2',
